6da8ec9f08a36c11372e234d42bdf6f78e346980
Vishwanath Krishnappanavar <Vishwanath.Krishnappanavar@infostretch.com>
2020-02-11 11:17:31 +0530

    starter files

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import * as ROUTES from '../../constants/routes';
+
+const Navigation = () => (
+  <div>
+    <h1>Navigation</h1>
+  </div>
+);
+
+export default Navigation;
---
+++ src/components/PasswordForget/index.js
@@ -0,0 +1,10 @@
+  
+import React from 'react';
+
+const PasswordForget = () => (
+  <div>
+    <h1>PasswordForget</h1>
+  </div>
+);
+
+export default PasswordForget;
